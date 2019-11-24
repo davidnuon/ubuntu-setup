5c99586e4051ce071643d63ecc53dadceb8f544f
davidnuon <davidnuongm@gmail.com>
2019-11-24 00:46:59 -0800

    Removed uneeded git configuration in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,22 +9,11 @@ fi
 
 CURRENT_USER=$1
 
-touch .env
-source .env
-
-GIT_USERNAME=${GIT_USERNAME:-user}
-GIT_EMAIL=${GIT_EMAIL:-user@example.org}
-
 echo "Grab updates..."
 apt-get update
 
 echo "Installing tools..."
 apt-get install -y python3-pip
-
-echo "Setting up git..."
-su $CURRENT_USER -c "git config --global user.name $GIT_USERNAME"
-su $CURRENT_USER -c "git config --global user.email $GIT_EMAIL"
-su $CURRENT_USER -c "git config --global core.editor vim"
 
 echo "Installing ansible..."
 pip3 install ansible
